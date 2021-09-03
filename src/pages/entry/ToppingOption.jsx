2126f9fb2aef51e1a5556b12c18d5f8d25fe3370
Yasuhiro Yoshida <892791+YasuhiroYoshida@users.noreply.github.com>
2021-09-03 18:58:52 +0300

    Test and code context for toppings subtotal

--- src/pages/entry/ToppingOption.jsx
+++ src/pages/entry/ToppingOption.jsx
@@ -1,6 +1,12 @@
+import { Form } from 'react-bootstrap';
 import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
 
-const ToppingOption = ({ name, imagePath }) => {
+const ToppingOption = ({ name, imagePath, updateItemCount }) => {
+  const changeHandler = (event) => {
+    updateItemCount(name, Number(event.target.checked));
+  };
+
   return (
     <Col xs={12} sm={6} md={4} lg={3} style={{ textAlign: 'center' }}>
       <img
@@ -8,6 +14,25 @@ const ToppingOption = ({ name, imagePath }) => {
         alt={`${name} topping`}
         style={{ width: '75%' }}
       />
+      <Form>
+        <Form.Group
+          controlId={`${name}-count`}
+          as={Row}
+          style={{ marginTop: '10px' }}
+        >
+          <Form.Label column xs={6} style={{ textAlign: 'right' }}>
+            {name}
+          </Form.Label>
+          <Col xs={5} style={{ textAlign: 'left' }}>
+            <Form.Control
+              type="checkbox"
+              defaultValue={false}
+              onChange={changeHandler}
+              label={name}
+            />
+          </Col>
+        </Form.Group>
+      </Form>
     </Col>
   );
 };
--- src/pages/entry/tests/totalUpdates.test.jsx
+++ src/pages/entry/tests/totalUpdates.test.jsx
@@ -22,3 +22,37 @@ test('update scoops subtotal when scoops change', async () => {
   userEvent.type(chocolateInput, '2');
   expect(scoopsSubtotal).toHaveTextContent('6.00');
 });
+
+test('update toppings subtotal when toppings change', async () => {
+  render(<Options optionType="toppings" />);
+
+  const toppingsSubtotal = screen.getByText('Toppings total: $', {
+    exact: false,
+  });
+  expect(toppingsSubtotal).toHaveTextContent(0.0);
+
+  const cherriesCheckbox = await screen.findByRole('checkbox', {
+    name: 'Cherries',
+  });
+  const mAndMsCheckbox = await screen.findByRole('checkbox', { name: 'M&Ms' });
+  const hotFudgeCheckbox = await screen.findByRole('checkbox', {
+    name: 'Hot fudge',
+  });
+
+  expect(cherriesCheckbox).not.toBeChecked();
+  expect(mAndMsCheckbox).not.toBeChecked();
+  expect(hotFudgeCheckbox).not.toBeChecked();
+  expect(toppingsSubtotal).toHaveTextContent('0.00');
+
+  userEvent.click(cherriesCheckbox);
+  expect(toppingsSubtotal).toHaveTextContent('1.50');
+
+  userEvent.click(mAndMsCheckbox);
+  expect(toppingsSubtotal).toHaveTextContent('3.00');
+
+  userEvent.click(hotFudgeCheckbox);
+  expect(toppingsSubtotal).toHaveTextContent('4.50');
+
+  userEvent.click(mAndMsCheckbox);
+  expect(toppingsSubtotal).toHaveTextContent('3.00');
+});
