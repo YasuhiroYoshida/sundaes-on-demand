2a10d5e67eddfa5ded303ad3eb58c0a68b4ea950
Yasuhiro Yoshida <892791+YasuhiroYoshida@users.noreply.github.com>
2021-09-04 16:38:23 +0300

    Fix checkbox alignment on ToppingOption

--- src/pages/entry/ToppingOption.jsx
+++ src/pages/entry/ToppingOption.jsx
@@ -1,6 +1,5 @@
 import { Form } from 'react-bootstrap';
 import Col from 'react-bootstrap/Col';
-import Row from 'react-bootstrap/Row';
 
 const ToppingOption = ({ name, imagePath, updateItemCount }) => {
   const changeHandler = (event) => {
@@ -8,33 +7,19 @@ const ToppingOption = ({ name, imagePath, updateItemCount }) => {
   };
 
   return (
-    <Col xs={12} sm={6} md={4} lg={3} style={{ textAlign: 'center' }}>
+    <Col xs={6} sm={4} md={3} lg={2} style={{ textAlign: 'center' }}>
       <img
         src={`http://localhost:3030${imagePath}`}
         alt={`${name} topping`}
         style={{ width: '75%' }}
       />
-      <Form>
-        <Form.Group
-          controlId={`${name}-count`}
-          as={Row}
-          style={{ marginTop: '10px' }}
-        >
-          <Form.Label column xs={6} style={{ textAlign: 'right' }}>
-            {name}
-          </Form.Label>
-          <Col xs={5} style={{ textAlign: 'left' }}>
-            <Form.Control
-              type="checkbox"
-              defaultValue={false}
-              onChange={changeHandler}
-              label={name}
-            />
-          </Col>
-        </Form.Group>
-      </Form>
+      <Form.Group
+        controlId={`${name}-topping-checkbox`}
+        className="d-flex justify-content-center"
+      >
+        <Form.Check type="checkbox" onChange={changeHandler} label={name} />
+      </Form.Group>
     </Col>
   );
 };
-
 export default ToppingOption;
